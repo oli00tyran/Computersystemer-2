d66b6f6053c6ba4b0442a31f5178e8effb4a3ee9
Oliver Brandt <olski-podolski@hotmail.com>
2022-09-12 21:51:39 +0200

    added function to sum to numbers

--- HelloWorld.c
+++ HelloWorld.c
@@ -8,13 +8,21 @@ int addNumbers(int a, int b)
 
 
 int main()
-{
+{   
+    int numberOne;
+    int numberTwo;
     int age;
     
     printf("How old are you?\n");
     scanf("%d", &age);
 
-    printf("You are %d years old", age);
+    printf("You are %d years old \n", age);
+
+    printf("Add these two numbers: \n");
+    scanf("%d %d", &numberOne, &numberTwo);
+    printf("The sum is %d \n", addNumbers(numberOne, numberTwo));
+
+
     return 0;
 }
 
